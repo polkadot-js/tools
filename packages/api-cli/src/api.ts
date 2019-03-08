be85705c026e8b6cc9bf573e933d60b18d57491c
Jaco Greeff <jacogr@gmail.com>
2019-03-08 15:11:25 +0100

    Allow for sending transactions

--- packages/api-cli/src/api.ts
+++ packages/api-cli/src/api.ts
@@ -3,12 +3,20 @@
 // of the Apache-2.0 license. See the LICENSE file for details.
 
 import yargs from 'yargs';
-import { ApiPromise, WsProvider } from '@polkadot/api';
-import { assert } from '@polkadot/util';
+import { ApiPromise, WsProvider, SubmittableResult } from '@polkadot/api';
+import { Keyring } from '@polkadot/keyring';
+import { assert, hexToU8a, isHex, stringToU8a } from '@polkadot/util';
 
 async function main (): Promise<void> {
-  const { _: [endpoint, ...params], sub, ws } = yargs
+  const log = (result: any) =>
+    console.log(JSON.stringify({ [method]: result }));
+
+  const { _: [endpoint, ...params], seed, sub, ws } = yargs
     .options({
+      seed: {
+        description: 'The account seed to use (for tx.* only)',
+        type: 'string'
+      },
       sub: {
         description: 'With this flag set, perofm subscriptions, running until exited with ^C',
         type: 'boolean'
@@ -27,20 +35,35 @@ async function main (): Promise<void> {
   const provider = new WsProvider(ws);
   const api = await ApiPromise.create({ provider });
   const [type, section, method] = endpoint.split('.');
+  const isTx = type === 'tx';
 
   assert(['derive', 'query', 'rpc', 'tx'].includes(type), `Expected one of derive, query, rpc, tx, found ${type}`);
+  assert(!isTx || seed, 'You need to specify an account seed with tx.*');
   assert((api as any)[type][section], `Cannot find ${type}.${section}`);
   assert((api as any)[type][section][method], `Cannot find ${type}.${section}.${method}`);
 
   const fn = (api as any)[type][section][method];
 
-  return sub
-    ? fn(...params, (result: any) => {
-      console.log(result);
-    })
-    : fn(...params).then((result: any) => {
-      console.log(result);
+  if (isTx && seed) {
+    const keyring = new Keyring();
+    const account = keyring.addFromSeed(
+      isHex(seed)
+        ? hexToU8a(seed)
+        : stringToU8a((seed as string).padEnd(32))
+    );
 
+    return fn(...params).signAndSend(account, (result: SubmittableResult) => {
+      log(result);
+
+      if (result.type === 'finalised') {
+        process.exit(0);
+      }
+    });
+  }
+
+  return sub
+    ? fn(...params, log)
+    : fn(...params).then(log).then(() => {
       process.exit(0);
     });
 }
