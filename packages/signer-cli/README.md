6b9dbeb7bbcd2dc31fe48ebd87b7007ede7eb192
Jaco Greeff <jacogr@gmail.com>
2019-09-10 14:43:04 +0200

    Simple offline  signer

---
+++ packages/signer-cli/README.md
@@ -0,0 +1,40 @@
+# @polkadot/signer-cli
+
+A simple cli interface foir offline siging
+
+## Create transactions to sign
+
+In a terminal run the `submit` command with the following form -
+
+`yarn run:signer submit --account <ss58> --ws <endpoint> <module.method> [param1] [...] [paramX]`
+
+For instance to make a transfer, we run the following command -
+
+`yarn run:signer submit --account 5HNHXTw65dTNVGRdYkxFUpKcvmUYQMZHcDHmSKpuC8pvVEaN --ws wss://poc3-rpc.polkadot.io/ balances.transfer 5DkQbYAExs3M2sZgT1Ec3mKfZnAQCL4Dt9beTCknkCUn5jzo 123 `
+
+On execution it will prompt us with -
+
+```
+Payload: 0x040300ff4a83f1...a8239139ff3ff7c3f6
+Signature>
+```
+
+The `Payload` is the hex that needs to be signed. Pastign  the hex signature (followed by `Enter`) submit it to the chain.
+
+## Sign transactions
+
+In a terminal, run the `sign` command with the following form -
+
+`yarn run:signer sign --account <ss58> --seed <suri> --type <ed25519|sr25519> <hex signature>`
+
+For instance to sign the transfer made above, we run -
+
+`yarn run:signer sign --account 5HNHXTw65dTNVGRdYkxFUpKcvmUYQMZHcDHmSKpuC8pvVEaN --seed "leaf .. rude" --type sr25519 0x040300ff4a83f1...a8239139ff3ff7c3f6`
+
+On executing, it will respond with -
+
+```
+Signature: 0xe6facf194a8e...413ce3155c2d1240b
+```
+
+Paste this signature into the  submission in the first terminal, and off we go.
---
+++ packages/signer-cli/src/signer.ts
@@ -0,0 +1,57 @@
+// Copyright 2019 @polkadot/signer-cli authors & contributors
+// This software may be modified and distributed under the terms
+// of the Apache-2.0 license. See the LICENSE file for details.
+
+import yargs from 'yargs';
+
+import cmdSign from './cmdSign';
+import cmdSubmit from './cmdSubmit';
+
+// retrieve and parse arguments - we do this globally, since this is a single command
+const { _: [command, ...params], account, seed, type, ws } = yargs
+  .usage('Usage: [options] <endpoint> <...params>')
+  .usage('Example: submit balances.transfer F7Gh 10000 --account D3AhD...wrx --ws wss://...')
+  .usage('Example: sign 0x123...789 --seed "..." --account D3AhD...wrx --crypto ed25519')
+  .usage('Example: --seed "//Alice" balances.transfer F7Gh 10000')
+  .wrap(120)
+  .options({
+    account: {
+      description: 'The actual address for the signer',
+      type: 'string',
+      required: true
+    },
+    seed: {
+      description: 'The account seed to use (sign only)',
+      type: 'string'
+    },
+    type: {
+      choices: ['ed25519', 'sr25519'],
+      default: 'sr25519',
+      description: 'The account crypto signature to use (sign only)',
+      type: 'string'
+    },
+    ws: {
+      description: 'The API endpoint to connect to, e.g. wss://poc3-rpc.polkadot.io (submit only)',
+      type: 'string'
+    }
+  })
+  .strict()
+  .argv;
+
+// our main entry point - from here we call out
+// eslint-disable-next-line @typescript-eslint/require-await
+async function main (): Promise<void> {
+  if (command === 'sign') {
+    return cmdSign(account, seed || '', type as 'ed25519', params);
+  } else if (command === 'submit') {
+    return cmdSubmit(account, ws || '', params);
+  }
+
+  throw new Error(`Unknown command '${command}' found, expected one of 'sign' or 'submit'`);
+}
+
+main().catch((error): void => {
+  console.error('ERROR:', error.message);
+
+  process.exit(1);
+});
