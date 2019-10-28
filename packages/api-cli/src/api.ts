ec1a834ac12d02a66df0373869b2a7fd3e972325
Ben Fenwick <benfenny@protonmail.com>
2019-10-28 11:28:49 -0400

    Lint fixes
    Removed explicit any and switched away from dot notation on the underscore.

--- packages/api-cli/src/api.ts
+++ packages/api-cli/src/api.ts
@@ -65,7 +65,7 @@ Example: --seed "//Alice" tx.balances.transfer F7Gh 10000`;
 const { _: [endpoint, ...params], info, seed, sign, sub, ws } = yargs
   .command('$0', usage)
   .middleware((argv) => {
-    argv['_'] = argv['_'].map((param: any) => {
+    argv._ = argv._.map((param) => {
       try {
         return JSON.parse(param);
       } catch (err) {
