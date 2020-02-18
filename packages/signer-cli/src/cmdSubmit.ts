87fc55284d6c48079ad381783a45fd14e35f7c59
Jaco Greeff <jacogr@gmail.com>
2020-02-18 10:59:28 +0100

    Bump api, use toHuman in logs (#101)

--- packages/signer-cli/src/cmdSubmit.ts
+++ packages/signer-cli/src/cmdSubmit.ts
@@ -2,35 +2,18 @@
 // This software may be modified and distributed under the terms
 // of the Apache-2.0 license. See the LICENSE file for details.
 
-import { Signer, SignerResult } from '@polkadot/api/types';
 import { SignerOptions } from '@polkadot/api/submittable/types';
-import { SignerPayloadRaw } from '@polkadot/types/types';
 
-import * as readline from 'readline';
 import { ApiPromise, WsProvider } from '@polkadot/api';
 import { assert } from '@polkadot/util';
 
-class RawSigner implements Signer {
-  public async signRaw ({ data }: SignerPayloadRaw): Promise<SignerResult> {
-    const rl = readline.createInterface({
-      input: process.stdin,
-      output: process.stdout
-    });
-
-    return new Promise((resolve): void => {
-      rl.question(`Payload: ${data}\nSignature> `, (signature) => {
-        resolve({ id: 1, signature });
-        rl.close();
-      });
-    });
-  }
-}
+import RawSigner from './RawSigner';
 
 function submitPreSignedTx (api: ApiPromise, tx: string): void {
   const extrinsic = api.createType('Extrinsic', tx);
 
-  api.rpc.author.submitAndWatchExtrinsic(extrinsic, result => {
-    console.log(JSON.stringify(result));
+  api.rpc.author.submitAndWatchExtrinsic(extrinsic, (result) => {
+    console.log(JSON.stringify(result.toHuman(), null, 2));
 
     if (result.isInBlock || result.isFinalized) {
       process.exit(0);
@@ -66,7 +49,7 @@ export default async function cmdSubmit (account: string, blocks: number | undef
   }
 
   await api.tx[section][method](...params).signAndSend(account, options, (result): void => {
-    console.log(JSON.stringify(result));
+    console.log(JSON.stringify(result.toHuman(), null, 2));
 
     if (result.isInBlock || result.isFinalized) {
       process.exit(0);
