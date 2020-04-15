f961eac541e1a646ce223c9fe8f35da21d40f213
Jaco Greeff <jacogr@gmail.com>
2020-04-15 20:01:43 +0200

    0.11 (#131)

--- babel.config.js
+++ babel.config.js
@@ -1,3 +1,7 @@
+// Copyright 2018-2020 @polkadot/tools authors & contributors
+// This software may be modified and distributed under the terms
+// of the Apache-2.0 license. See the LICENSE file for details.
+
 module.exports = {
   extends: '@polkadot/dev/config/babel'
 };
