2f3a6735c968a8b607d1f2acc3e3d411092008c5
YJ <yjkimjunior@gmail.com>
2020-06-29 12:40:33 +0800

    fix(#40): validate payload before signing (#143)
    * fix: validate payload before signing
    
    * fix: lint

--- packages/signer-cli/src/cmdSign.ts
+++ packages/signer-cli/src/cmdSign.ts
@@ -3,7 +3,7 @@
 // of the Apache-2.0 license. See the LICENSE file for details.
 
 import { Keyring } from '@polkadot/keyring';
-import { hexToU8a, u8aToHex, u8aConcat } from '@polkadot/util';
+import { assert, hexToU8a, isHex, u8aToHex, u8aConcat } from '@polkadot/util';
 import { cryptoWaitReady } from '@polkadot/util-crypto';
 
 type Curves = 'ed25519' | 'sr25519';
@@ -14,6 +14,9 @@ const curvePrefixes: {[key in Curves]: [number] } = {
 };
 
 export default async function cmdSign (_: string, seed: string, type: Curves, [payload]: string[]): Promise<void> {
+  assert(payload && payload.length > 0, 'Cannot sign empty payload. Please check your input and try again.');
+  assert(isHex(payload), 'Payload must be supplied as a hex string. Please check your input and try again.');
+
   await cryptoWaitReady();
 
   const keyring = new Keyring({ type });
