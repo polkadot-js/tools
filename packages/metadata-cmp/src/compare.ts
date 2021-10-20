a76b3c0d503390d1dd7a5dec0333e206b666781a
Alexander Popiak <alexander.popiak@parity.io>
2021-10-20 14:24:11 +0200

    add more spec diffing to the metadata comparison

--- packages/metadata-cmp/src/compare.ts
+++ packages/metadata-cmp/src/compare.ts
@@ -97,7 +97,10 @@ async function main (): Promise<number> {
   const lvl5 = lvl1 + 4 * lvlInc;
   const chunkSize = 5;
 
-  log(lvl1, 'Spec', 'version:', createCompare(verA.specVersion.toNumber(), verB.specVersion.toNumber()));
+  log(lvl1, 'Spec', 'name:', createCompare(verA.specName.toString(), verB.specName.toString()));
+  log(lvl1, '', 'spec_version:', createCompare(verA.specVersion.toNumber(), verB.specVersion.toNumber()));
+  log(lvl1, '', 'transaction_version:',
+    createCompare(verA.transactionVersion.toNumber(), verB.transactionVersion.toNumber()));
   log(lvl1, 'Metadata', 'version:', createCompare(metaA.version, metaB.version));
 
   const mA = a.pallets.map(({ name }) => name.toString());
