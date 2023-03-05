d0c7c5747fe6cf4df89bb6c85d00bc8681654362
Jaco <jacogr@gmail.com>
2023-03-05 11:55:22 +0200

    Adjust command import

--- packages/json-serve/src/cjs/cjs/index.js
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env node
-// Copyright 2018-2023 @polkadot/json-serve authors & contributors
-// SPDX-License-Identifier: Apache-2.0
-
-require('../runcli.js');
--- packages/vanitygen/src/cjs/cjs/index.js
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env node
-// Copyright 2018-2023 @polkadot/vanitygen authors & contributors
-// SPDX-License-Identifier: Apache-2.0
-
-require('../runcli.js');
