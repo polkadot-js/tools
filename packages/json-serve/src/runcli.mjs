050ffc63b4c566b5739b69cfbe90ff0146580475
Jaco <jacogr@gmail.com>
2023-03-08 10:50:27 +0200

    Executable header to scripts (& explicit mjs)

---
+++ packages/json-serve/src/runcli.mjs
@@ -0,0 +1,5 @@
+#!/usr/bin/env node
+// Copyright 2017-2023 @polkadot/json-serve authors & contributors
+// SPDX-License-Identifier: Apache-2.0
+
+import './runcli.js';
---
+++ packages/metadata-cmp/src/runcli.mjs
@@ -0,0 +1,5 @@
+#!/usr/bin/env node
+// Copyright 2017-2023 @polkadot/metadata-cmp authors & contributors
+// SPDX-License-Identifier: Apache-2.0
+
+import './runcli.js';
