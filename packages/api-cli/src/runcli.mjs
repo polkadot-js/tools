744807b41bb4fd3fc6f3ffa8de5e84a6acc91a7a
Jaco <jacogr@gmail.com>
2024-01-08 15:00:05 +0200

    2024

--- packages/api-cli/src/runcli.mjs
+++ packages/api-cli/src/runcli.mjs
@@ -1,5 +1,5 @@
 #!/usr/bin/env node
-// Copyright 2017-2023 @polkadot/api-cli authors & contributors
+// Copyright 2017-2024 @polkadot/api-cli authors & contributors
 // SPDX-License-Identifier: Apache-2.0
 
 import './runcli.js';
